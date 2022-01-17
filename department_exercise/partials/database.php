6f65d69b406d1bac290d2657769e1b2f882e81e3
giulio30795 <giulioincarbone95@gmail.com>
2022-01-17 14:33:02 +0100

    check

---
+++ department_exercise/partials/database.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'university');
+
+$conn = new mysqli (DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+var_dump($conn);
